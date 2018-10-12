c0c9165591ee7805ae4e8a2ccd1f48727b92098b
developerprince <N01416165H@students.nust.ac.zw>
2018-10-12 16:02:07 +0200

    Changes

---
+++ source/link.ts
@@ -0,0 +1,15 @@
+import {PythonShell} from 'python-shell';
+ 
+let options = {
+  mode: 'text',
+  pythonPath: '',
+  pythonOptions: ['-u'], // get print results in real-time
+  scriptPath: 'controllers/',
+  args: ['value1', 'value2', 'value3', 'value4', 'value5', 'value6','value7', 'value8']
+};
+ 
+PythonShell.run('Concrete_Comp_Test_Beta.py', options, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  console.log('results: %j', results);
+});
