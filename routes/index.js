80f8db36bfeb5d8a334178eb94137fd0da74b7cd
developerprince <N01416165H@students.nust.ac.zw>
2018-10-14 13:05:23 +0200

    improvement

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,10 @@ router.get('/ccst', function(req, res, next) {
   res.render('ccst');
 });
 
+router.get('/cst', function(req, res, next) {
+  res.render('cst');
+});
+
 router.get('/contact', function(req, res, next) {
   res.render('contact');
 });
