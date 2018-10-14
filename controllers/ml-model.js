3e9b20bf5178f347720aae85542f354631d3cdba
developerprince <N01416165H@students.nust.ac.zw>
2018-10-14 15:02:39 +0200

    ml-model refinement

--- controllers/ml-model.js
+++ controllers/ml-model.js
@@ -1,17 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var python_shell_1 = require("python-shell");
-var options = {
-    mode: 'text',
-    pythonPath: '',
-    pythonOptions: ['-u'],
-    scriptPath: 'controllers',
-    args: ['value1', 'value2', 'value3', 'value4', 'value5', 'value6', 'value7', 'value8']
+exports.mlmodel = function (a, b, c, d, e, f, g, h) {
+    var options = {
+        mode: 'text',
+        pythonPath: '',
+        pythonOptions: ['-u'],
+        scriptPath: 'controllers',
+        args: [a, b, c, d, e, f, g, h]
+    };
+    python_shell_1.PythonShell.run('Run_Model.py', options, function (err, results) {
+        if (err)
+            throw err;
+        // results is an array consisting of messages collected during execution
+        return (results);
+    });
 };
-python_shell_1.PythonShell.run('Concrete_Comp_Test_Beta2.py', options, function (err, results) {
-    if (err)
-        throw err;
-    // results is an array consisting of messages collected during execution
-    console.log('results: %j', results);
-});
-module.exports = python_shell_1.PythonShell;
--- source/ml-model.ts
+++ source/ml-model.ts
@@ -1,17 +1,20 @@
 import {PythonShell} from 'python-shell';
  
-let options = {
-  mode: 'text',
-  pythonPath: '',
-  pythonOptions: ['-u'], // get print results in real-time
-  scriptPath: 'controllers',
-  args: ['value1', 'value2', 'value3', 'value4', 'value5', 'value6','value7', 'value8']
+
+exports.mlmodel = function(a, b, c, d, e, f, g, h) {
+  let options = {
+    mode: 'text',
+    pythonPath: '',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: 'controllers',
+    args: [a, b, c, d, e, f, g, h]
+  };
+  
+  PythonShell.run('Run_Model.py', options, function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    return( results);
+  });
 };
  
-PythonShell.run('Run_Model.py', options, function (err, results) {
-  if (err) throw err;
-  // results is an array consisting of messages collected during execution
-  console.log('results: %j', results);
-});
 
-module.exports = PythonShell
