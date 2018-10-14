2d336467c41f453d513c478f85b3e57773964deb
developerprince <N01416165H@students.nust.ac.zw>
2018-10-14 13:03:49 +0200

    ml-model js built

---
+++ controllers/ml-model.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var python_shell_1 = require("python-shell");
+var options = {
+    mode: 'text',
+    pythonPath: '',
+    pythonOptions: ['-u'],
+    scriptPath: '',
+    args: ['value1', 'value2', 'value3', 'value4', 'value5', 'value6', 'value7', 'value8']
+};
+python_shell_1.PythonShell.run('Concrete_Comp_Test_Beta2.py', options, function (err, results) {
+    if (err)
+        throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+});
