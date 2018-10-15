8b25dc5e2bdc4ce4a965cc5596f47314288d211b
developerprince <N01416165H@students.nust.ac.zw>
2018-10-15 06:31:50 +0200

    body - parser

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 var router = express.Router();
 var ml = require('../controllers/ml-model');
+var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,6 +19,19 @@ router.get('/services', function(req, res, next) {
 
 router.get('/ccst', function(req, res, next) {
   res.render('ccst');
+});
+
+router.post('/ccst/:water/:cement/:fagg/:cagg/:sps/:bfurn/:fash/:day', function(req, res, next) {
+  var water = req.param.water;
+  var cement = req.param.cement;
+  var fagg = req.param.fagg;
+  var cagg = req.param.cagg;
+  var sps = req.param.sps;
+  var bfurn = req.param.bfurn;
+  var fash = req.param.fash;
+  var day = req.param.day;
+
+  ml.mlmodel(water, cement, fagg, cagg, sps, bfurn, fash, day);
 });
 
 router.get('/cst', function(req, res, next) {
